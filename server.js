75e4a4165ec544918ff2116ad8ff05db8bf0315a
Xiaochen Du <duxiaochen1996@outlook.com>
2016-08-27 04:41:02 +0000

    finally solved the thorny issue that was a pain in the rear!

--- server.js
+++ server.js
@@ -6,25 +6,32 @@ var port = process.env.PORT || 8080
 
 app.set('views', path.join(__dirname, 'templates'))
 app.set('view engine', 'jade')
+app.use(express.static(path.join(__dirname, 'templates')))
+
 app.get('/', function(req, res) {
     res.render('index')
 })
 
 app.get('/:date', function(req, res) {
     var input = req.params.date
+    if (/[a-zA-Z]/g.test(input) == false) {
+        input = +req.params.date
+    }
+    console.log(input)
     
-    /*function dpad(val) {
-        return val < 10 ? "0" + val : val 
-    }*/
     function isDate(val) {
     var d = new Date(val);
-    if (!isNaN(d)) {
+    console.log(d)
+    if (!isNaN(d.getTime())) {
         return {
-            unixdate: d.valueOf(),
-            natural: /*dpad(d.getMonth()+1) + "-" + dpad(d.getDate()) + "-" + dpad(d.getFullYear()) + "fulldate" + */d.toDateString().slice(4,15)
+            unixdate: d.getTime(),
+            natural: d.toDateString().slice(4,15)
         }
     } else {
-        return false
+        return {
+            unixdate: null,
+            natural: null
+        }
     }
 }
     res.send(isDate(input))
