62624dd9db90526c8409240d95d0c7d809257687
Xiaochen Du <duxiaochen1996@outlook.com>
2016-08-27 01:35:15 +0000

    minor update

--- server.js
+++ server.js
@@ -2,8 +2,30 @@ var express = require('express')
 var app = express()
 
 app.get('/', function(req, res) {
-    res.send('Hello World!')
+    res.send('Hello World!)
 })
+
+app.get('/:date', function(req, res) {
+    var input = req.params.date
+    
+    /*function dpad(val) {
+        return val < 10 ? "0" + val : val 
+    }*/
+    function isDate(val) {
+    var d = new Date(val);
+    if (!isNaN(d)) {
+        return {
+            unixdate: d.valueOf(),
+            readabledate: /*dpad(d.getMonth()+1) + "-" + dpad(d.getDate()) + "-" + dpad(d.getFullYear()) + "fulldate" + */d.toDateString().slice(4,15)
+        }
+    } else {
+        return false
+    }
+}
+    res.send(isDate(input))
+       
+})
+
 app.listen(8080, function() {
     console.log('Listening on port 8080')
 })
